3a524065c48facd986f7ccb257ef61aeb9b49a17
dalsong00 <index159@naver.com>
2023-05-18 13:22:15 +0900

    Add : 날씨 정보 화면에 표시

--- js/weather.js
+++ js/weather.js
@@ -4,6 +4,7 @@ const getLoaction = () => {
     maximumAge: 30000,
     timeout: 27000,
   };
+
   navigator.geolocation.getCurrentPosition(
     getLocationSuc,
     getLocationErr,
@@ -24,18 +25,31 @@ const getLocationErr = (error) => {
 
 const getWeatherInfo = (latitude, longitude) => {
   const APIKEY = "91ab9442f47f179e8d008c3a609c5e3e";
-  const lat = latitude;
-  const lon = longitude;
 
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKEY}&units=metric`
   )
     .then((res) => res.json())
     .then((result) => {
       const weatherInfo = result;
-      console.log(result);
-      return weatherInfo;
+      const temp = weatherInfo.main.temp;
+      const weather = weatherInfo.weather[0].main;
+
+      document.querySelector(".weather").textContent = weatherValue[weather];
+      document.querySelector(".temp").textContent = temp.toFixed(1) + " °C";
     });
+};
+
+const weatherValue = {
+  Clear: "맑음",
+  Rain: "비",
+  Thunderstorm: "뇌우",
+  Snow: "눈",
+  Mist: "옅은 안개",
+  Drizzle: "이슬비",
+  Clouds: "흐림",
+  Fog: "안개",
+  Haze: "실안개",
 };
 
 getLoaction();
